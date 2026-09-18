58bb4d5d232e858e5763afdd68ad0a4e5abc9238
iulo <liulong0130@gmail.com>
2026-09-18 11:32:26 +0800

    Add dark mode preview support

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import 'virtual:uno.css'
 import './style.css'
 import App from './App.vue'
 
+const debugTheme = new URLSearchParams(location.search).get('theme')
+if (debugTheme === 'dark' || debugTheme === 'light') document.documentElement.dataset.theme = debugTheme
+
 createApp(App).mount('#app')
